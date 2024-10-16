05c87aa3281177496b4fdd84e3becfcceecfc722
roberto dias <devv-robert@outlook.com>
2024-10-15 21:38:21 -0300

    Create stack.java
    add: simple stack algorithm.

---
+++ java/DataStructs/stack.java
@@ -0,0 +1,12 @@
+class Stack<t> {
+  private int len;
+  private int top;
+  private t[] dat;
+
+  public void push(T value) {
+    if (this.top > 0 && this.top < this.len) 
+       this.t[++this.top] = value;
+  }
+  
+  
+}
