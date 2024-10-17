aab7a935b750f18f53b07d8b9d91780652194b1a
K-Zoldyck <devv-robert@outlook.com>
2024-10-17 14:35:06 -0300

    add: basic code of dinamic stack

--- DataStructs/src/com/dinamics/Stack.java
+++ DataStructs/src/com/dinamics/Stack.java
@@ -1,10 +1,40 @@
 
 /**
- *
+ * @date 17/10/2024, 14:23
  * @author K-Zoldyck
  */
 
 package com.dinamics;
-public class Stack {
+class Node<E> {
+    E      value;
+    Node<E> before;
     
+    public Node(E value, E before) {
+        this.value = value;
+        this.before  =  (Node<E>) before;
+    }
+} 
+
+public class Stack<E> {
+    private Node<E>  top = null;
+    
+    public void push(E value) {
+        if ( this.top == null ) {
+            this.top = new Node(value, null);
+            return;
+        }
+        Node node = new Node(value,this.top);
+        this.top = node;
+    }
+    
+    public void pop() {
+        if (this.top != null ) 
+            this.top = this.top.before;
+    }
+    
+    public E get() {
+        if (this.top != null )
+            return this.top.value;
+        return null;
+    }
 }
